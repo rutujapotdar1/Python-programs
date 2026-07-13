318441561993140131043a1d8852e973c095aa4f
Rutuja potdar <rutuja@gmail.com>
2026-07-13 10:17:34 +0530

    python codes

---
+++ day10.py
@@ -0,0 +1,18 @@
+# students = ["Rutuja", "Rohit","Rahul"]
+# print(students)
+
+# fruits = ["Apple", "Banana", "Mango", "Orange"]
+# print("List of fruits")
+# print(fruits)
+
+# colors = ("Red", "Blue", "Pink", "Green")
+# print("fourth color is:")
+# print(colors[3])
+
+# numbers = {10, 20, 30, 20, 40, 30, 10}
+# print("Set of numbers")
+# print(numbers)
+
+  def welcome():
+ print("welcome to python")
+welcome()
---
+++ day12.py
@@ -0,0 +1,27 @@
+# fruits = ["Apple", "Banana", "Mango", "Orange"]
+# print("list of fruits")
+# print(fruits)
+
+# colors = ("Red", "Blue", "Pink", "Green")
+# print("Foruth color is:")
+# print(colors[3])
+
+# numbers = {10, 20, 30, 20, 40 ,10}
+# print("set of numbers")
+# print(numbers)
+
+#student = {
+ #   "name": "Rutuja",
+  #  "age": 19,
+   # "city": "pune"
+#}
+#print(student)
+
+#def welcome():
+ #   print("Welcome to python")
+#welcome()
+
+def print_name():
+    for i in range(5):
+        print("Virat")
+print_name() 
